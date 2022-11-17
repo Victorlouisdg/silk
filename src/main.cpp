c36389c9798addd2fda3dc118243be432e6f2951
victorlouisdg <victorlouisdg@gmail.com>
2022-11-17 14:16:17 +0100

    add additional quantity visualizations

--- src/main.cpp
+++ src/main.cpp
@@ -12,47 +12,78 @@
 using namespace geometrycentral;
 using namespace geometrycentral::surface;
 
-int main() {
+void addSmoothestVertexDirectionField(VertexPositionGeometry &geometry,
+                                      SurfaceMesh &mesh,
+                                      polyscope::SurfaceMesh &psMesh)
+{
+  VertexData<Vector2> field = computeSmoothestVertexDirectionField(geometry);
+  geometry.requireVertexTangentBasis();
+  VertexData<Vector3> vBasisX(mesh);
+  for (Vertex v : mesh.vertices()) {
+    vBasisX[v] = geometry.vertexTangentBasis[v][0];
+  }
+  psMesh.setVertexTangentBasisX(vBasisX);
+  psMesh.addVertexIntrinsicVectorQuantity("vectors", field);
+}
+
+void addFaceAreas(VertexPositionGeometry &geometry, SurfaceMesh &mesh, polyscope::SurfaceMesh &psMesh)
+{
+  geometry.requireFaceAreas();
+
+  FaceData<double> areas(mesh);
+  int i = 0;
+  for (Face f : mesh.faces()) {
+    areas[i] = geometry.faceAreas[f];
+    i++;
+  }
+  psMesh.addFaceScalarQuantity("areas", areas);
+}
+
+void addFaceNormals(VertexPositionGeometry &geometry, SurfaceMesh &mesh, polyscope::SurfaceMesh &psMesh)
+{
+  geometry.requireFaceNormals();
+  psMesh.addFaceVectorQuantity("normals", geometry.faceNormals);
+}
+
+void addCurvatures(VertexPositionGeometry &geometry, SurfaceMesh &mesh, polyscope::SurfaceMesh &psMesh)
+{
+  geometry.requireVertexGaussianCurvatures();
+  geometry.requireVertexMeanCurvatures();
+  geometry.requireVertexMinPrincipalCurvatures();
+  geometry.requireVertexMaxPrincipalCurvatures();
+
+  psMesh.addVertexScalarQuantity("gaussian_curvatures", geometry.vertexGaussianCurvatures);
+  psMesh.addVertexScalarQuantity("mean_curvatures", geometry.vertexMeanCurvatures);
+  psMesh.addVertexScalarQuantity("min_principal_curvatures", geometry.vertexMinPrincipalCurvatures);
+  psMesh.addVertexScalarQuantity("max_principal_curvatures", geometry.vertexMaxPrincipalCurvatures);
+}
+
+/*Dihedral edge*/
+
+void addEdgeDihedralAngle(VertexPositionGeometry &geometry, SurfaceMesh &mesh, polyscope::SurfaceMesh &psMesh)
+{
+  geometry.requireEdgeDihedralAngles();
+  psMesh.addEdgeScalarQuantity("dihedral_angles", geometry.edgeDihedralAngles);
+}
+
+int main()
+{
   torch::Tensor tensor = torch::rand({2, 3});
   std::cout << tensor << std::endl;
 
   std::unique_ptr<SurfaceMesh> mesh;
   std::unique_ptr<VertexPositionGeometry> geometry;
-  std::tie(mesh, geometry) = readManifoldSurfaceMesh("spot.obj");
+  std::tie(mesh, geometry) = readManifoldSurfaceMesh("../meshes/shirt_wrinkled.obj");
 
-  for (Vertex v : mesh->vertices()) {
-    std::cout << "Vertex " << v << " has degree " << v.degree() << "\n";
-    for (Face fn : v.adjacentFaces()) {
-      std::cout << "  incident on face " << fn << "\n";
-    }
-  }
+  polyscope::init();
 
-  polyscope::init(); // initialize the gui
+  auto *psMesh = polyscope::registerSurfaceMesh("my mesh", geometry->vertexPositions, mesh->getFaceVertexList());
 
-  // add the mesh to the gui
-  auto *psMesh = polyscope::registerSurfaceMesh(
-      "my mesh", geometry->vertexPositions, mesh->getFaceVertexList());
+  addSmoothestVertexDirectionField(*geometry, *mesh, *psMesh);
+  addFaceAreas(*geometry, *mesh, *psMesh);
+  addFaceNormals(*geometry, *mesh, *psMesh);
+  addCurvatures(*geometry, *mesh, *psMesh);
+  addEdgeDihedralAngle(*geometry, *mesh, *psMesh);
 
-  VertexData<Vector2> field = computeSmoothestVertexDirectionField(*geometry);
-  geometry->requireVertexTangentBasis();
-  VertexData<Vector3> vBasisX(*mesh);
-  for (Vertex v : mesh->vertices()) {
-    vBasisX[v] = geometry->vertexTangentBasis[v][0];
-  }
-  psMesh->setVertexTangentBasisX(vBasisX);
-  psMesh->addVertexIntrinsicVectorQuantity("vectors", field);
-
-  IntrinsicGeometryInterface &geometry2 = *geometry;
-  geometry2.requireFaceAreas();
-
-  FaceData<double> areas(*mesh);
-  int i = 0;
-  for (Face f : mesh->faces()) {
-
-    // Managed array holding quantity
-    areas[i] = geometry2.faceAreas[f];
-    i++;
-  }
-  psMesh->addFaceScalarQuantity("areas", areas);
-  polyscope::show(); // pass control to the gui until the user exits
+  polyscope::show();
 }
