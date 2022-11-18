c52564116e098f648deb3cf45f3ffbeda531da4b
victorlouisdg <victorlouisdg@gmail.com>
2022-11-18 07:13:39 +0100

    single triangle forces as grad of energy

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 using namespace geometrycentral;
 using namespace geometrycentral::surface;
 
+using namespace torch;
 using namespace torch::indexing;
 
 void addSmoothestVertexDirectionField(VertexPositionGeometry &geometry,
@@ -83,34 +84,62 @@ void visualizeMesh() {
   polyscope::show();
 }
 
-torch::Tensor deformationGradient(torch::Tensor positions, torch::Tensor positions_uv) {
-  // Read out the rows. 
-  torch::Tensor u0 = positions_uv.index({0, Slice()});
-  torch::Tensor u1 = positions_uv.index({1, Slice()});
-  torch::Tensor u2 = positions_uv.index({2, Slice()});
-  return u0;
+Tensor deformationGradient(Tensor positions, Tensor positions_uv) {
+  // Read out the rows.
+  Tensor u0 = positions_uv.index({0, Slice()});
+  Tensor u1 = positions_uv.index({1, Slice()});
+  Tensor u2 = positions_uv.index({2, Slice()});
+
+  Tensor Dm = torch::column_stack({u1 - u0, u2 - u0});
+  Tensor Dm_inv = torch::inverse(Dm);
+
+  Tensor x0 = positions.index({0, Slice()});
+  Tensor x1 = positions.index({1, Slice()});
+  Tensor x2 = positions.index({2, Slice()});
+  Tensor Ds = torch::column_stack({x1 - x0, x2 - x0});
+
+  return torch::matmul(Ds, Dm_inv);
 }
 
 int main() {
+  // Torch part
+  Tensor positions_orignal = torch::tensor({{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.5, 0.0}},
+                                           torch::requires_grad());
+  Tensor positions = torch::tensor({{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 1.0, 0.0}}, torch::requires_grad());
+
+  std::cout << positions << std::endl;
+
+  Tensor positions_uv = positions_orignal.index({Slice(), Slice(None, 2)});  // positions[:, :2];
+  std::cout << positions_uv << std::endl;
+
+  Tensor F = deformationGradient(positions, positions_uv);
+  Tensor wu = F.index({Slice(), 0});
+  Tensor wv = F.index({Slice(), 1});
+
+  Tensor Cu = torch::norm(wu) - 1.0;
+  Tensor Cv = torch::norm(wv) - 1.0;
+
+  Tensor Eu = 0.5 * Cu * Cu;
+  Tensor Ev = 0.5 * Cv * Cv;
+
+  Tensor E = Eu + Ev;
+
+  E.backward();
+  positions.grad();
+
+  std::cout << wu << std::endl;
+  std::cout << wv << std::endl;
+
+  std::cout << Eu << std::endl;
+  std::cout << positions.grad() << std::endl;
+
+  // PolyScope part
   std::vector<size_t> triangle{0, 1, 2};
   std::vector<std::vector<size_t>> triangles{triangle};
 
   Vector3 v0{0.0, 0.0, 0.0};
   Vector3 v1{1.0, 0.0, 0.0};
   Vector3 v2{0.5, 0.5, 0.0};
-
-  torch::Tensor positions = torch::zeros({3, 3});
-  positions.index_put_({1, 0}, 1.0);
-  positions.index_put_({2, 0}, 0.5);
-  positions.index_put_({2, 1}, 0.5);
-  std::cout << positions << std::endl;
-
-
-  torch::Tensor positions_uv = positions.index({Slice(), Slice(None, 2)}); // positions[:, :2];
-  std::cout << positions_uv << std::endl;
-
-  torch::Tensor deformationGradient_ = deformationGradient(positions, positions_uv);
-  std::cout << deformationGradient_ << std::endl;
 
   std::vector<Vector3> vertexCoordinates{v0, v1, v2};
 
@@ -120,9 +149,9 @@ int main() {
   std::unique_ptr<VertexPositionGeometry> geometry;
   std::tie(mesh, geometry) = makeManifoldSurfaceMeshAndGeometry(simpleMesh.polygons, simpleMesh.vertexCoordinates);
 
-  // polyscope::init();
-  // polyscope::view::upDir = polyscope::UpDir::ZUp;
-  // auto *psMesh = polyscope::registerSurfaceMesh("my mesh", geometry->vertexPositions, mesh->getFaceVertexList());
-  // addFaceNormals(*geometry, *mesh, *psMesh);
-  // polyscope::show();
+  polyscope::init();
+  polyscope::view::upDir = polyscope::UpDir::ZUp;
+  auto *psMesh = polyscope::registerSurfaceMesh("my mesh", geometry->vertexPositions, mesh->getFaceVertexList());
+  addFaceNormals(*geometry, *mesh, *psMesh);
+  polyscope::show();
 }
