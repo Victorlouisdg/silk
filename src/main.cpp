98a94cfd632d14bb9094f6e98c0aab678c96f09c
victorlouisdg <victorlouisdg@gmail.com>
2022-11-18 11:37:07 +0100

    initial attempt at higher order gradients

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,12 @@
 #include "geometrycentral/surface/surface_mesh_factories.h"
 
 #include <iostream>
-#include <torch/torch.h>
 
 #include "polyscope/polyscope.h"
 #include "polyscope/surface_mesh.h"
 
 using namespace geometrycentral;
 using namespace geometrycentral::surface;
-
-using namespace torch;
-using namespace torch::indexing;
 
 void addSmoothestVertexDirectionField(VertexPositionGeometry &geometry,
                                       SurfaceMesh &mesh,
@@ -84,55 +80,7 @@ void visualizeMesh() {
   polyscope::show();
 }
 
-Tensor deformationGradient(Tensor positions, Tensor positions_uv) {
-  // Read out the rows.
-  Tensor u0 = positions_uv.index({0, Slice()});
-  Tensor u1 = positions_uv.index({1, Slice()});
-  Tensor u2 = positions_uv.index({2, Slice()});
-
-  Tensor Dm = torch::column_stack({u1 - u0, u2 - u0});
-  Tensor Dm_inv = torch::inverse(Dm);
-
-  Tensor x0 = positions.index({0, Slice()});
-  Tensor x1 = positions.index({1, Slice()});
-  Tensor x2 = positions.index({2, Slice()});
-  Tensor Ds = torch::column_stack({x1 - x0, x2 - x0});
-
-  return torch::matmul(Ds, Dm_inv);
-}
-
 int main() {
-  // Torch part
-  Tensor positions_orignal = torch::tensor({{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 0.5, 0.0}},
-                                           torch::requires_grad());
-  Tensor positions = torch::tensor({{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.5, 1.0, 0.0}}, torch::requires_grad());
-
-  std::cout << positions << std::endl;
-
-  Tensor positions_uv = positions_orignal.index({Slice(), Slice(None, 2)});  // positions[:, :2];
-  std::cout << positions_uv << std::endl;
-
-  Tensor F = deformationGradient(positions, positions_uv);
-  Tensor wu = F.index({Slice(), 0});
-  Tensor wv = F.index({Slice(), 1});
-
-  Tensor Cu = torch::norm(wu) - 1.0;
-  Tensor Cv = torch::norm(wv) - 1.0;
-
-  Tensor Eu = 0.5 * Cu * Cu;
-  Tensor Ev = 0.5 * Cv * Cv;
-
-  Tensor E = Eu + Ev;
-
-  E.backward();
-  positions.grad();
-
-  std::cout << wu << std::endl;
-  std::cout << wv << std::endl;
-
-  std::cout << Eu << std::endl;
-  std::cout << positions.grad() << std::endl;
-
   // PolyScope part
   std::vector<size_t> triangle{0, 1, 2};
   std::vector<std::vector<size_t>> triangles{triangle};
